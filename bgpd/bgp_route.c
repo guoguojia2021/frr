61f6ddd0c2dac4aa166fa5378a0c7019994aa7ac
gongjian <gongjian@localhost.localdomain>
2019-04-30 02:27:38 +0000

    set BATTR_RMAP_IPV6_PREFER_GLOBAL_CHANGED flag when set ipv6 nexthop prefer global
    Reviewers: P604087, 俱东
    
    Reviewed By: P604087, 俱东
    
    Subscribers: 俱东, P604087
    
    Differential Revision: https://aone.alibaba-inc.com/code/D875103
    
    Signed-off-by: guozhongfeng <guozhongfeng.gzf@alibaba-inc.com>

--- bgpd/bgp_route.c
+++ bgpd/bgp_route.c
@@ -4252,6 +4252,8 @@ int bgp_update(struct peer *peer, const struct prefix *p, uint32_t addpath_id,
 		if(bgp_debug_update(peer, p, NULL, 1))
 			zlog_debug("%s: set ipv6 next-hop prefer-global by default", peer->host);
 		new_attr.mp_nexthop_prefer_global = TRUE;
+		SET_FLAG(new_attr.rmap_change_flags,
+			BATTR_RMAP_IPV6_PREFER_GLOBAL_CHANGED);
 	}
 
 	/* Apply incoming route-map.
