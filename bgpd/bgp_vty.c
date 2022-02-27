284d61269215af28d2fc001dc4f54e41d756b4ed
guozhongfeng <guozhongfeng.gzf@alibaba-inc.com>
2022-02-27 06:11:47 +0000

    Modify the import-check command no need to require the underlying prefixto exist in the rib

--- bgpd/bgp_vty.c
+++ bgpd/bgp_vty.c
@@ -79,12 +79,8 @@
 #endif
 
 FRR_CFG_DEFAULT_BOOL(BGP_IMPORT_CHECK,
-	{
-		.val_bool = false,
-			.match_profile = "traditional",
-			.match_version = "< 7.4",
-	},
-	{ .val_bool = true },
+	{ .val_bool = true, .match_profile = "datacenter", },
+	{ .val_bool = false },
 );
 FRR_CFG_DEFAULT_BOOL(BGP_SHOW_HOSTNAME,
 	{ .val_bool = true, .match_profile = "datacenter", },
