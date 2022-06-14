218bc8f50fe7c448bb7ae7d7e9ed75e98c8c1361
Donald Sharp <sharpd@nvidia.com>
2022-06-14 14:45:01 -0400

    zebra: Add `%pNG` to zebra print routines
    Add `%pNG` so that a nexthop group can be displayed in debugs/logs
    such that it can provide useful information.
    
    Signed-off-by: Donald Sharp <sharpd@nvidia.com>

--- zebra/zebra_nhg.h
+++ zebra/zebra_nhg.h
@@ -402,6 +402,10 @@ extern bool zebra_nhg_segdependents_is_empty(const struct nhg_hash_entry *nhe);
 extern void zebra_nhg_seg_free(struct nhg_hash_entry *nhe);
 extern void zebra_nhe_change_gateway_address(struct nexthop *nexthop);
 
+#ifdef _FRR_ATTRIBUTE_PRINTFRR
+#pragma FRR printfrr_ext "%pNG" (const struct nhg_hash_entry *)
+#endif
+
 #ifdef __cplusplus
 }
 #endif
