16cdfda66596fdb0f7758e8f0f85c7cae6ad6017
Philippe Guibert <philippe.guibert@6wind.com>
2022-07-20 14:53:20 +0200

    pathd: add a zebra stop handler
    This handler code is used to stop zebra related contexts.
    
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

--- pathd/path_zebra.h
+++ pathd/path_zebra.h
@@ -33,4 +33,6 @@ void path_zebra_init(struct thread_master *master);
 void path_zebra_add_srv6_policy(struct srte_policy *policy,
 			      struct srte_candidate_group *candidate_group);
 void path_zebra_delete_srv6_policy(struct srte_policy *policy);
+void path_zebra_stop(void);
+
 #endif /* _FRR_PATH_MPLS_H_ */
