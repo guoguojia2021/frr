a855a00d7963817e77d87d9a82e08efd44d9ffae
guozhongfeng <guozhongfeng.gzf@alibaba-inc.com>
2022-08-15 21:42:58 +0800

    Revert "ospf6d: Fix memory leak for `show ipv6 ospf6 zebra json`"
    This reverts commit 22c8eda3578a412cd3d4ab4980786e4e3fcc052a.

--- ospf6d/ospf6_zebra.c
+++ ospf6d/ospf6_zebra.c
@@ -353,7 +353,9 @@ DEFUN(show_zebra,
 		json_object_object_add(json_zebra, "redistribute", json_array);
 		json_object_object_add(json, "zebraInformation", json_zebra);
 
-		vty_json(vty, json);
+		vty_out(vty, "%s\n",
+			json_object_to_json_string_ext(
+				json, JSON_C_TO_STRING_PRETTY));
 	} else {
 		vty_out(vty, "Zebra Infomation\n");
 		vty_out(vty, "  fail: %d\n", zclient->fail);
