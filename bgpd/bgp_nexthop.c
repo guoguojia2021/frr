8d686125a024ecb68102841ebe48636ea3c0fef1
Donald Sharp <sharpd@nvidia.com>
2022-09-30 14:58:21 -0400

    bgpd: Fix `show bgp nexthop A.B.C.D`
    The issuing of `show bgp nexthop A.B.C.D` fails even if that
    nexthop exists:
    
    eva# show bgp nexthop 192.168.119.120
    specified nexthop does not have entry
    
    Fixed:
    
    eva# show bgp nexthop 192.168.119.120
     192.168.119.120 valid [IGP metric 0], #paths 0, peer 192.168.119.120
      if enp39s0
      Last update: Fri Sep 30 14:55:13 2022
    
      Paths:
    
    Signed-off-by: Donald Sharp <sharpd@nvidia.com>
    Discussed-in: Merge-Request 14367580 1/2, URL: https://code.alibaba-inc.com/SONiC/alibgp/codereview/14367580

--- bgpd/bgp_nexthop.c
+++ bgpd/bgp_nexthop.c
@@ -955,8 +955,7 @@ static int show_ip_bgp_nexthop_table(struct vty *vty, const char *name,
             vty_out(vty, "error nexthop type\n");
     		return CMD_WARNING;
         }
-		
-		bnc = bnc_find(tree[family2afi(nhop.family)], &nhop, 0);
+		bnc = bnc_find(&(*tree)[family2afi(nhop.family)], &nhop, 0);
 		if (!bnc) {
 			vty_out(vty, "specified nexthop does not have entry\n");
 			return CMD_SUCCESS;
