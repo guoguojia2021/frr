81129860339bb319222b50b2adf82aca6eb523c4
Philippe Guibert <philippe.guibert@6wind.com>
2022-12-19 15:39:00 +0100

    isisd: show isis route .. json adds the algorithm value
    When walking over all the flex-algorithm routes, it is
    difficult to know which route is associated to which
    algorithm: add "algorithm" attribute to the json object.
    
    Output example:
    
    > ubuntu2004(config-router)# do show isis route algorithm  json
    > [
    >   {
    >     "area":"1",
    >     "algorithm":128,
    >     "level-1":{
    >      ]
    >    },
    >    "level-2":{
    >      "area":"1"
    >    },
    >    "vrf_id":0
    >  },
    >  {
    >    "area":"1",
    >    "algorithm":129,
    >    "level-1":{
    >      "area":"1",
    
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>
    Signed-off-by: Louis Scalbert <louis.scalbert@6wind.com>

--- isisd/isis_spf.c
+++ isisd/isis_spf.c
@@ -2449,6 +2449,7 @@ static void show_isis_route_common(struct vty *vty, int levels,
 			jstr = json_object_new_string(
 				area->area_tag ? area->area_tag : "null");
 			json_object_object_add(*json, "area", jstr);
+			json_object_int_add(*json, "algorithm", algo);
 		} else {
 			vty_out(vty, "Area %s:\n",
 				area->area_tag ? area->area_tag : "null");
