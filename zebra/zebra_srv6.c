b153ac25907a31499e2aee7f64dea2d1900d23f6
qingyan.gw <qingyan.gw@alibaba-inc.com>
2023-02-17 10:19:51 +0800

    support ecore2.5 end-x         Link: https://code.alibaba-inc.com/SONiC/alibgp/codereview/11698072
        * support ecore2.5 end-x

--- zebra/zebra_srv6.c
+++ zebra/zebra_srv6.c
@@ -648,8 +648,7 @@ void zebra_srv6_local_sid_add(struct srv6_locator *locator, struct seg6_sid *sid
     strncpy(ctx.vrfName, sid->vrfName, VRF_ALIASNAMESIZ + 1);
 
     if (CHECK_FLAG(vrf->status, VRF_ACTIVE)) {
-		if (act != ZEBRA_SEG6_LOCAL_ACTION_END_X)
-            zebra_Db_Set_SRV6_LOCAL_SID(&result_sid, vrf->name, act, &ctx, sid->ifname, &sid->nexthop);
+        zebra_Db_Set_SRV6_LOCAL_SID(&result_sid, vrf->name, act, &ctx, sid->ifname, &sid->nexthop);
 		zebra_route_add(&result_sid, vrf, act, &ctx);
 	}
     
