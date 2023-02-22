f9bdb5890bdb3285328249a81e702aad39b8ce23
guozhongfeng <guozhongfeng.gzf@alibaba-inc.com>
2023-02-22 17:01:30 +0800

    fix delete opcode, route still takes sid

--- lib/zclient.c
+++ lib/zclient.c
@@ -1261,7 +1261,7 @@ int zapi_srv6_del_sid_decode(struct stream *s,
         STREAM_GET(&tmpsid.vrfName, s, vrf_name_len);
         tmpsid.vrfName[vrf_name_len] = '\0';
         for (ALL_LIST_ELEMENTS(sidlist, node, nnode, sid)) {
-            if (prefix_match((struct prefix *)(&tmpsid.ipv6Addr.prefix), (struct prefix *)(&sid->ipv6Addr.prefix))){
+            if (prefix_match((struct prefix *)(&tmpsid.ipv6Addr), (struct prefix *)(&sid->ipv6Addr))){
                 list_delete_node(sidlist, node);
                 srv6_locator_sid_free(sid);
                 break;
