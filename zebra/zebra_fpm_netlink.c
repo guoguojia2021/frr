ce6d2eb32db1dce957685b4f374c36f3230746e0
Donald Sharp <sharpd@nvidia.com>
2023-05-19 09:54:05 -0400

    zebra: Do not allow old FPM to access freed memory after shutdown
    On shutdown, the old FPM queues up dests to be sent to
    the FPM listener.  This is done through the rib_shutdown
    hook.  Which is called when the table that the routes are
    stored in are being deleted.  This dest has pointers
    to the rnode.  The rnode has pointers to the table it
    is associated with as well as the table->info pointer for
    the zebra data associated with this table.
    
    The FPM after this attempts to tell this to it's listener
    via events.  Unfortunately the zvrf, table_id and nl_pid
    was being grabbed from memory that had been freed!  Since
    all this can be grabbed from memory that has not been freed
    on shutdown let's switch over to using that instead of freed
    memory for gathering data.
    
    Signed-off-by: Donald Sharp <sharpd@nvidia.com>

--- zebra/zebra_fpm_netlink.c
+++ zebra/zebra_fpm_netlink.c
@@ -439,27 +439,15 @@ static int netlink_route_info_fill(struct netlink_route_info *ri, int cmd,
 				   rib_dest_t *dest, struct route_entry *re)
 {
 	struct nexthop *nexthop;
-	struct rib_table_info *table_info =
-		rib_table_info(rib_dest_table(dest));
-	struct zebra_vrf *zvrf = table_info->zvrf;
 
 	memset(ri, 0, sizeof(*ri));
 
 	ri->prefix = rib_dest_prefix(dest);
 	ri->af = rib_dest_af(dest);
 
-	if (zvrf && zvrf->zns)
-		ri->nlmsg_pid = zvrf->zns->netlink_dplane_out.snl.nl_pid;
+	ri->nlmsg_pid = pid;
 
 	ri->nlmsg_type = cmd;
-	/*
-	 * Revert above code temporarily until
-	 * tableid is supported by sonic fpmsyncd
-	 * See FRR #4365, SONIC #15, and #3280, #3704
-	 * for more details, aloha
-	 */
-	ri->rtm_table = zvrf_id(rib_dest_vrf(dest));
-
 	ri->rtm_protocol = RTPROT_UNSPEC;
 
 	/*
@@ -473,6 +461,8 @@ static int netlink_route_info_fill(struct netlink_route_info *ri, int cmd,
 		zfpm_debug("%s: Expected non-NULL re pointer", __func__);
 		return 0;
 	}
+
+	ri->rtm_table = re->table;
 
 	ri->rtm_protocol = netlink_proto_from_route_type(re->type);
 	ri->rtm_type = RTN_UNICAST;
