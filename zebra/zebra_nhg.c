8f6ed7334b79a22cf107538e2e9b048190898699
hanyu.zly <hanyu.zly@alibaba-inc.com>
2023-09-18 20:26:36 +0800

    zebra crash when receive route without nh from kernel

--- zebra/zebra_nhg.c
+++ zebra/zebra_nhg.c
@@ -403,7 +403,7 @@ void zebra_nhe_init(struct nhg_hash_entry *nhe, afi_t afi,
 			break;
 		}
 	}
-	if (nh->nh_srv6 && CHECK_FLAG(nh->alibgp_flags, NEXTHOP_FLAG_SRV6_RVIP))
+	if (nh && nh->nh_srv6 && CHECK_FLAG(nh->alibgp_flags, NEXTHOP_FLAG_SRV6_RVIP))
 		SET_FLAG(nhe->flags, NEXTHOP_GROUP_BYPASS_KERNEL);
 }
 
