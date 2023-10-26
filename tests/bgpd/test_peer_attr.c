287c12b0fd4e0c25a598ec7c12cb9eb93c3a6dbb
Donatas Abraitis <donatas@opensourcerouting.org>
2023-10-26 17:20:28 +0300

    tests: Drop broken unit test for extended-nexthop capability
    extended-nexthop capability can't be unset to interface-based peers.
    
    Anyway, this is always silently ignored:
    
    ```
    ✖ [test] peer\capability extended-nexthop
    ► prepare: initialize bgp test environment
    ► case 01: set peer-flag [capability extended-nexthop] on [IP-TEST]
    ► error: execution of command [no neighbor IP-TEST capability extended-nexthop] has failed with code [13]
    failed
    ```
    
    Signed-off-by: Donatas Abraitis <donatas@opensourcerouting.org>
    Discussed-in: Merge-Request 21613116 4/4, URL: https://code.alibaba-inc.com/SONiC/alibgp/codereview/21613116

--- tests/bgpd/test_peer_attr.c
+++ tests/bgpd/test_peer_attr.c
@@ -309,13 +309,6 @@ static struct test_peer_attr test_peer_attrs[] = {
 		.type = PEER_AT_GLOBAL_FLAG,
 	},
 	{
-		.cmd = "capability extended-nexthop",
-		.u.flag = PEER_FLAG_CAPABILITY_ENHE,
-		.type = PEER_AT_GLOBAL_FLAG,
-		.o.invert_peer = true,
-		.o.use_iface_peer = true,
-	},
-	{
 		.cmd = "description",
 		.peer_cmd = "description FRR Peer",
 		.group_cmd = "description FRR Group",
