f982b7cb303ad0dd0e3eba4eb272be1ed094db2a
Donald Sharp <sharpd@nvidia.com>
2024-02-08 12:22:09 -0500

    zebra: Move nhg reinstallation into if_up proper
    The function call in to zebra_interface_nhg_reinstall
    is an action that takes place on interface up events
    *not* when the connected addresses are added to
    a system.  this will prevent this function being
    called when new connected interfaces come alive
    that is an independent operation of the interface
    coming up.
    
    Signed-off-by: Donald Sharp <sharpd@nvidia.com>

--- zebra/interface.c
+++ zebra/interface.c
@@ -1016,6 +1016,14 @@ void if_up(struct interface *ifp)
 	/* Install connected routes to the kernel. */
 	if_install_connected(ifp);
 
+	/*
+	 * Interface associated NHG's have been deleted on
+	 * interface down events, now that this interface
+	 * is coming back up, let's resync the zebra -> dplane
+	 * nhg's so that they can be continued to be used.
+	 */
+	zebra_interface_nhg_reinstall(ifp);
+
 	/* Handle interface up for specific types for EVPN. Non-VxLAN interfaces
 	 * are checked to see if (remote) neighbor entries need to be installed
 	 * on them for ARP suppression.
