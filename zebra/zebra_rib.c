05cda61c2364f2f1bd35370d3cc9936348ccca9f
guozhongfeng.gzf <guozhongfeng.gzf@alibaba-inc.com>
2024-04-03 09:47:22 +0800

    fix static route bug

--- zebra/zebra_rib.c
+++ zebra/zebra_rib.c
@@ -4206,6 +4206,7 @@ void rib_delete(afi_t afi, safi_t safi, vrf_id_t vrf_id, int type,
 				     rtnh = rtnh->next)
 					UNSET_FLAG(rtnh->flags,
 						   NEXTHOP_FLAG_FIB);
+				rib_uninstall_kernel(rn, fib);
 
 				/*
 				 * This is a non FRR route
