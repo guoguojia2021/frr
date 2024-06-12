f7089071bc8948c16fc8f626346c773e427d5c3b
yuezhou.jk <yuezhou.jk@alibaba-inc.com>
2024-06-12 12:33:50 +0000

    Fix static routing was not configured when configuring a non-compressed locator
    Discussed-in: Merge-Request 17059355, URL: https://code.alibaba-inc.com/SONiC/alibgp/codereview/17059355

--- zebra/zebra_srv6.c
+++ zebra/zebra_srv6.c
@@ -626,7 +626,7 @@ void zebra_srv6_local_sid_add(struct srv6_locator *locator, struct seg6_sid *sid
 
     strncpy(ctx.vrfName, sid->vrfName, VRF_ALIASNAMESIZ + 1);
 
-	if (act == ZEBRA_SEG6_LOCAL_ACTION_END_X)
+	if (act == ZEBRA_SEG6_LOCAL_ACTION_END_X && locator->compress == true)
 	{
 		for (ALL_LIST_ELEMENTS_RO(locator->sid_ua_ecmps, node, sid_ua_ecmp_index)) {
 			if (IPV6_ADDR_SAME(&sid_ua_ecmp_index->ipv6Addr.prefix, &result_sid)) {
@@ -644,7 +644,7 @@ void zebra_srv6_local_sid_add(struct srv6_locator *locator, struct seg6_sid *sid
 	}
 
     if (CHECK_FLAG(vrf->status, VRF_ACTIVE)) {
-		if (act == ZEBRA_SEG6_LOCAL_ACTION_END_X)
+		if (act == ZEBRA_SEG6_LOCAL_ACTION_END_X && locator->compress == true)
 			zebra_Db_Set_SRV6_LOCAL_ENDX_SID(&result_sid, vrf->name, act, &ctx, sid_ua_ecmp->sid_ua_params);
 		else
         	zebra_Db_Set_SRV6_LOCAL_SID(&result_sid, vrf->name, act, &ctx, sid->ifname, &sid->nexthop);
