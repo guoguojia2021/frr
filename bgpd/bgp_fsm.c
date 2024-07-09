8b6c8e11ed65b8361e7bd7609766d80c70f87ec2
guozhongfeng.gzf <guozhongfeng.gzf@alibaba-inc.com>
2024-07-09 15:12:56 +0800

    Revert "bgpd: fix addressing information of non established outgoing sessions"
    This reverts commit ed5132a42e58e77abb3837f8c98fdf979994150c.

--- bgpd/bgp_fsm.c
+++ bgpd/bgp_fsm.c
@@ -2027,22 +2027,6 @@ static int bgp_connect_fail(struct peer *peer)
 	return (bgp_stop(peer));
 }
 
-/* after connect is called(), getpeername is able to return
- * port and address on non established streams
- */
-static void bgp_connect_in_progress_update_connection(struct peer *peer)
-{
-	bgp_getsockname(peer);
-	if (!peer->su_remote && !BGP_CONNECTION_SU_UNSPEC(peer->connection)) {
-		/* if connect initiated, then dest port and dest addresses are well known */
-		peer->su_remote = sockunion_dup(&peer->connection->su);
-		if (sockunion_family(peer->su_remote) == AF_INET)
-			peer->su_remote->sin.sin_port = htons(peer->port);
-		else if (sockunion_family(peer->su_remote) == AF_INET6)
-			peer->su_remote->sin6.sin6_port = htons(peer->port);
-	}
-}
-
 /* This function is the first starting point of all BGP connection. It
  * try to connect to remote peer with non-blocking IO.
  */
@@ -2158,8 +2142,6 @@ int bgp_start(struct peer *peer)
 				 peer->fd);
 			return -1;
 		}
-		bgp_connect_in_progress_update_connection(peer);
-
 		/*
 		 * - when the socket becomes ready, poll() will signify POLLOUT
 		 * - if it fails to connect, poll() will signify POLLHUP
