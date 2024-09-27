265108da0207b4db65f943d659d53e5891704c80
galadriel.zyq <galadriel.zyq@alibaba-inc.com>
2024-09-27 14:12:31 +0800

    fix: show ip[v6] access-list ... json
    This is based on FRR community.
    
    Similar to the commits before, the same problem is in
    command "show ip[v6] access-list [NAME] json" as well,
    that is, the invalid json display.
    Fix it.
    
    PRLink: https://github.com/FRRouting/frr/pull/15973
    
    Added-by: Yuqing Zhao <galadriel.zyq@alibaba-inc.com>

--- vtysh/vtysh.c
+++ vtysh/vtysh.c
@@ -3686,6 +3686,124 @@ DEFUN (show_ipv6_prefix_list_summary,
 	return CMD_SUCCESS;
 }
 
+static void show_access_list_send(afi_t afi, const char *access_list, bool json)
+{
+	unsigned int i;
+	bool first = true;
+	char command_line[128];
+
+	if (afi == AFI_IP)
+		snprintf(command_line, sizeof(command_line),
+				 "do show ip access-list ");
+	else if (afi == AFI_IP6)
+		snprintf(command_line, sizeof(command_line),
+				 "do show ipv6 access-list ");
+	if (access_list)
+		strlcat(command_line, access_list, sizeof(command_line));
+	if (json) {
+		strlcat(command_line, " json", sizeof(command_line));
+		vty_out(vty, "{");
+	}
+
+	for (i = 0; i < array_size(vtysh_client); i++) {
+		const struct vtysh_client *client = &vtysh_client[i];
+		bool is_connected = true;
+
+		if (!CHECK_FLAG(client->flag, VTYSH_ACCESS_LIST_SHOW))
+			continue;
+		
+		for (; client; client = client->next)
+			if (client->fd < 0)
+				is_connected = false;
+		
+		if (!is_connected)
+			continue;
+		
+		if (json && !first)
+			vty_out(vty, ",");
+		else
+			first = false;
+		
+		if (json)
+			vty_out(vty, "\"%s\":", vtysh_client[i].name);
+
+		vtysh_client_execute_name(vtysh_client[i].name, command_line);
+	}
+
+	if (json)
+		vty_out(vty, "}\n");
+}
+
+DEFUN (show_ip_access_list,
+	   show_ip_access_list_cmd,
+	   "show ip access-list [json]",
+	   SHOW_STR
+	   IP_STR
+	   "List IP access lists\n"
+	   JSON_STR)
+{
+	bool uj = use_json(argc, argv);
+
+	show_access_list_send(AFI_IP, NULL, !!uj);
+	return CMD_SUCCESS;
+}
+
+DEFUN (show_ip_access_list_name,
+	   show_ip_access_list_name_cmd,
+	   "show ip access-list <ACCESSLIST4_NAME> [json]",
+	   SHOW_STR
+	   IP_STR
+	   "List IP access lists\n"
+	   "IP access-list name\n"
+	   JSON_STR)
+{
+	bool uj = use_json(argc, argv);
+	int idx_name = 3;
+	char *acl_name = NULL;
+
+	if (argc >= 5 || (argc == 4 && !uj)) {
+		acl_name = argv[idx_name]->arg;
+	}
+
+	show_access_list_send(AFI_IP, acl_name, !!uj);
+	return CMD_SUCCESS;
+}
+
+DEFUN (show_ipv6_access_list,
+	   show_ipv6_access_list_cmd,
+	   "show ipv6 access-list [json]",
+	   SHOW_STR
+	   IPV6_STR
+	   "List IPv6 access lists\n"
+	   JSON_STR)
+{
+	bool uj = use_json(argc, argv);
+
+	show_access_list_send(AFI_IP6, NULL, !!uj);
+	return CMD_SUCCESS;
+}
+
+DEFUN (show_ipv6_access_list_name,
+	   show_ipv6_access_list_name_cmd,
+	   "show ipv6 access-list <ACCESSLIST6_NAME> [json]",
+	   SHOW_STR
+	   IP_STR
+	   "List IPv6 access lists\n"
+	   "IPv6 access-list name\n"
+	   JSON_STR)
+{
+	bool uj = use_json(argc, argv);
+	int idx_name = 3;
+	char *acl_name = NULL;
+
+	if (argc >= 5 || (argc == 4 && !uj)) {
+		acl_name = argv[idx_name]->arg;
+	}
+
+	show_access_list_send(AFI_IP6, acl_name, !!uj);
+	return CMD_SUCCESS;
+}
+
 DEFUN (vtysh_integrated_config,
        vtysh_integrated_config_cmd,
        "service integrated-vtysh-config",
@@ -4998,6 +5116,10 @@ void vtysh_init_vty(void)
 	install_element(ENABLE_NODE, &show_ipv6_prefix_list_seq_cmd);
 	install_element(ENABLE_NODE, &show_ipv6_prefix_list_detail_cmd);
 	install_element(ENABLE_NODE, &show_ipv6_prefix_list_summary_cmd);
+	install_element(ENABLE_NODE, &show_ip_access_list_cmd);
+	install_element(ENABLE_NODE, &show_ip_access_list_name_cmd);
+	install_element(ENABLE_NODE, &show_ipv6_access_list_cmd);
+	install_element(ENABLE_NODE, &show_ipv6_access_list_name_cmd);
 
 	/* "write terminal" command. */
 	install_element(ENABLE_NODE, &vtysh_write_terminal_cmd);
