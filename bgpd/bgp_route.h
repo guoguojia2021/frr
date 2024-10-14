d2b286df74eb50818bdf936b56dfbd48ec189f33
huangchengjiu.hcj <huangchengjiu.hcj@alibaba-inc.com>
2024-10-14 15:50:17 +0800

    set BGP_PATH_BACKUP_SELECTED bit 15

--- bgpd/bgp_route.h
+++ bgpd/bgp_route.h
@@ -308,10 +308,9 @@ struct bgp_path_info {
 #define BGP_PATH_COUNTED (1 << 10)
 #define BGP_PATH_MULTIPATH (1 << 11)
 #define BGP_PATH_MULTIPATH_CHG (1 << 12)
-#define BGP_PATH_RIB_ATTR_CHG (1 << 13)
+#define BGP_PATH_BACKUP_SELECTED (1 << 13)
 #define BGP_PATH_ANNC_NH_SELF (1 << 14)
 #define BGP_PATH_LINK_BW_CHG (1 << 15)
-#define BGP_PATH_BACKUP_SELECTED (1 << 16)
 
 	/* BGP route type.  This can be static, RIP, OSPF, BGP etc.  */
 	uint8_t type;
