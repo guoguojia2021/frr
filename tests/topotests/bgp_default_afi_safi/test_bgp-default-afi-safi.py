9deafc14df8925e812413b02d89590f130f3bb7d
hanyu.zly <hanyu.zly@alibaba-inc.com>
2024-11-05 17:12:54 +0800

    add basic testcase for FRR topotest

--- tests/topotests/bgp_default_afi_safi/test_bgp-default-afi-safi.py
+++ tests/topotests/bgp_default_afi_safi/test_bgp-default-afi-safi.py
@@ -30,7 +30,7 @@ sys.path.append(os.path.join(CWD, "../"))
 from lib.topogen import Topogen, TopoRouter, get_topogen
 from lib.common_config import step
 
-pytestmark = [pytest.mark.bgpd]
+pytestmark = [pytest.mark.bgpd, pytest.mark.esr]
 
 
 def build_topo(tgen):
