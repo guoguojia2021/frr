95221f3a1d4c721e97ec67aba695bd9a04209735
guozhongfeng.gzf <guozhongfeng.gzf@alibaba-inc.com>
2025-01-07 16:12:47 +0800

    Fix srv6 te policy cpath update error

--- zebra/zebra_srte.c
+++ zebra/zebra_srte.c
@@ -317,6 +317,8 @@ static struct nexthop *zebra_nhg_seg_update_nexthop(struct nexthop *nexthop,
 	resolved_hop->flags = 0;
 	SET_FLAG(resolved_hop->flags, NEXTHOP_FLAG_ACTIVE);
 	SET_FLAG(resolved_hop->flags, NEXTHOP_FLAG_SRV6_TUNNEL);
+	if (CHECK_FLAG(nexthop->flags, NEXTHOP_FLAG_DUPLICATE))
+		SET_FLAG(resolved_hop->flags, NEXTHOP_FLAG_DUPLICATE);
 	if (is_backup)
 		SET_FLAG(resolved_hop->flags, NEXTHOP_FLAG_IS_BACKUP);
 
