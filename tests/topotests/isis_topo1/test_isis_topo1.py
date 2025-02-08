88840169a9e03668d05314c675d21b36c5b428aa
guozhongfeng.gzf <guozhongfeng.gzf@alibaba-inc.com>
2025-02-08 17:23:31 +0800

    Remove isis topotest case

--- tests/topotests/isis_topo1/test_isis_topo1.py
+++ tests/topotests/isis_topo1/test_isis_topo1.py
@@ -41,7 +41,7 @@ from lib.topogen import Topogen, TopoRouter, get_topogen
 from lib.topolog import logger
 
 
-pytestmark = [pytest.mark.isisd, pytest.mark.esr]
+pytestmark = [pytest.mark.isisd]
 
 VERTEX_TYPE_LIST = [
     "pseudo_IS",
