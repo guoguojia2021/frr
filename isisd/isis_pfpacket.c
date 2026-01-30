cf71755f2489500d7689adcb60e3df369d9cede7
guozhongfeng.gzf <guozhongfeng.gzf@alibaba-inc.com>
2026-01-30 10:26:20 +0800

    Set the message send as non blocking

--- isisd/isis_pfpacket.c
+++ isisd/isis_pfpacket.c
@@ -441,7 +441,7 @@ int isis_send_pdu_bcast(struct isis_circuit *circuit, int level)
 	iov[1].iov_base = circuit->snd_stream->data;
 	iov[1].iov_len = stream_get_endp(circuit->snd_stream);
 
-	if (sendmsg(circuit->fd, &msg, 0) < 0) {
+	if (sendmsg(circuit->fd, &msg, MSG_DONTWAIT) < 0) {
 		zlog_warn("IS-IS pfpacket: could not transmit packet on %s: %s",
 			  circuit->interface->name, safe_strerror(errno));
 		if (ERRNO_IO_RETRY(errno))
