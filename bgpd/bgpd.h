a50d3be03f7f85def05172ff8b3ae7b3ae3d3e27
Yijiao Qin <y.qin@alibaba-inc.com>
2026-02-27 01:22:34 +0000

    modify PEER_FLAG_BGP_LS_IPV4 shift lenght

--- bgpd/bgpd.h
+++ bgpd/bgpd.h
@@ -1737,8 +1737,8 @@ struct peer {
 #define PEER_FLAG_MAX_PREFIX_OUT            (1U << 27) /* outgoing maximum prefix */
 #define PEER_FLAG_MAX_PREFIX_FORCE          (1U << 28) /* maximum-prefix <num> force */
 #define PEER_FLAG_DISABLE_ADDPATH_RX        (1U << 29) /* disable-addpath-rx */
-#define PEER_FLAG_BGP_LS_IPV4			  (1ULL << 35)
-#define PEER_FLAG_BGP_LS_IPV6			  (1ULL << 36)
+#define PEER_FLAG_BGP_LS_IPV4			    (1U << 30)
+#define PEER_FLAG_BGP_LS_IPV6			    (1U << 31)
 	enum bgp_addpath_strat addpath_type[AFI_MAX][SAFI_MAX];
 
 	/* MD5 password */
