59539e7ef6757fd8556d2c4ffb087b0fb4dcd252
Mark Stapp <mjs@cisco.com>
2026-03-23 09:29:31 -0400

    bgpd: call init and term funcs for LS attr admin_group
    There's an admin group struct embedded in the BGP LS attr;
    it needs to be init'd and freed.
    
    Reported-by: Haruto Kimura (Stella) <harutokimura0608@gmail.com>
    Signed-off-by: Mark Stapp <mjs@cisco.com>

--- bgpd/bgp_ls_nlri.c
+++ bgpd/bgp_ls_nlri.c
@@ -495,6 +495,7 @@ struct bgp_ls_attr *bgp_ls_attr_alloc(void)
 	struct bgp_ls_attr *attr;
 
 	attr = XCALLOC(MTYPE_BGP_LS_ATTR, sizeof(struct bgp_ls_attr));
+	admin_group_init(&attr->ext_admin_group);
 	return attr;
 }
 
@@ -502,6 +503,8 @@ void bgp_ls_attr_free(struct bgp_ls_attr *attr)
 {
 	if (!attr)
 		return;
+
+	admin_group_term(&attr->ext_admin_group);
 
 	XFREE(MTYPE_BGP_LS_ATTR, attr->node_name);
 	XFREE(MTYPE_BGP_LS_ATTR, attr->isis_area_id);
